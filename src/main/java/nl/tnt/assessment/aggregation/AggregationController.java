e792534936909a567c189a3df6a2892f2f9dc3be
baovuvu <baovu.vu@gmail.com>
2023-05-06 11:20:59 +0200

    setting the results from the clients asynchronously (workoraound!)

--- src/main/java/nl/tnt/assessment/aggregation/AggregationController.java
+++ src/main/java/nl/tnt/assessment/aggregation/AggregationController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("aggregation")
@@ -17,6 +21,8 @@ public class AggregationController {
     private final PricingClient pricingClient;
     private final TrackClient trackClient;
 
+    private final List<Function<AggregationResponse, AggregationResponse>> setters = List.of(this::setShipments, this::setTrack, this::setPricing);
+
     public AggregationController(ShipmentClient shipmentClient, PricingClient pricingClient, TrackClient trackClient) {
         this.shipmentClient = shipmentClient;
         this.pricingClient = pricingClient;
@@ -25,22 +31,36 @@ public class AggregationController {
 
     @GetMapping
     public AggregationResponse get(String shipments, String track, String pricing) {
-//        final Map<String, AggregationResponse> = Map.of(shipments)
-        return AggregationResponse.builder()
-            .shipments(shipmentClient.get(getList(shipments)))
-            .pricing(pricingClient.get(getList(pricing)))
-            .track(trackClient.get(getList(track)))
+        final AggregationResponse response = AggregationResponse.builder()
+            .shipmentsParam(shipments)
+            .trackParam(track)
+            .pricingParam(pricing)
             .build();
+        // setting the results from the clients asynchronously:
+        setters.stream().parallel().forEach(f -> f.apply(response));
+        return response;
+//        return AggregationResponse.builder()
+//            .shipments(shipmentClient.get(getList(shipments)))
+//            .pricing(pricingClient.get(getList(pricing)))
+//            .track(trackClient.get(getList(track)))
+//            .build();
     }
 
-    private AggregationResponse setShipments(String param){
-        return AggregationResponse.builder().shipments(shipmentClient.get(getList(param))).build();
+    private AggregationResponse setShipments(AggregationResponse response) {
+        return response.setShipments(shipmentClient.get(getList(response.getShipmentsParam())));
+    }
+
+    private AggregationResponse setTrack(AggregationResponse response) {
+        return response.setTrack(trackClient.get(getList(response.getTrackParam())));
+    }
+
+    private AggregationResponse setPricing(AggregationResponse response) {
+        return response.setPricing(pricingClient.get(getList(response.getPricingParam())));
     }
 
     private static List<String> getList(String commaSeparatedValues) {
         return Optional.ofNullable(commaSeparatedValues)
             .map(values -> Arrays.stream(values.split(","))
-//                .filter(StringUtils::hasText)
                 .distinct()
                 .toList())
             .orElse(Collections.emptyList());
--- src/main/java/nl/tnt/assessment/aggregation/AggregationResponse.java
+++ src/main/java/nl/tnt/assessment/aggregation/AggregationResponse.java
@@ -1,7 +1,9 @@
 package nl.tnt.assessment.aggregation;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import lombok.experimental.Accessors;
 
 import java.util.List;
 import java.util.Map;
@@ -9,9 +11,17 @@ import java.util.Map;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @Data
 @Builder
+@Accessors(chain = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // hibernate, serializer
 @AllArgsConstructor(access = AccessLevel.PRIVATE) // builder
 public class AggregationResponse {
+
+    @JsonIgnore
+    private String shipmentsParam;
+    @JsonIgnore
+    private String pricingParam;
+    @JsonIgnore
+    private String trackParam;
 
     private Map<String, List<String>> shipments;
     private Map<String, Float> pricing;
