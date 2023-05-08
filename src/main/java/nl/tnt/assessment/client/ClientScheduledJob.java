2736defe5b6b2333177700e7c581db7049e833a5
baovuvu <baovu.vu@gmail.com>
2023-05-08 09:38:22 +0200

    Schedule

--- src/main/java/nl/tnt/assessment/client/ClientScheduledJob.java
+++ src/main/java/nl/tnt/assessment/client/ClientScheduledJob.java
@@ -1,13 +1,12 @@
 package nl.tnt.assessment.client;
 
 import org.quartz.JobExecutionContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@SuppressWarnings("rawtypes")
 @Service
 public class ClientScheduledJob extends QuartzJobBean {
 
